0c8ffa16468f1f4ceec9f6d51ecb23426e2d8ac5
mmoghal <mmoghal29@gmail.com>
2023-07-12 00:11:13 -0400

    Added All rights reserved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import styles from './Footer.module.css'; // import styles
+import styles from './Footer.module.css'; 
 
 function Footer() {
+  const year = new Date().getFullYear(); 
+
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/mmoghal" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="https://www.linkedin.com/in/mmoghal" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="https://stackoverflow.com/users/mmoghal" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+      <div>
+        <a href="https://github.com/mmoghal" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/mmoghal" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/mmoghal" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+      </div>
+      <div>
+        {year} © nRm. All rights reserved.
+      </div>
     </footer>
   );
 }
